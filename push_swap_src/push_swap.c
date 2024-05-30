a4f0d65a67195b103cfca96d688a38800fc78500
Bovic101 <Victorbolanle72@gmail.com>
2024-05-30 11:12:00 +0200

    implementation of my main function

--- push_swap_src/push_swap.c
+++ push_swap_src/push_swap.c
@@ -6,62 +6,57 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 00:38:40 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/05/21 05:04:43 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/05/30 11:05:28 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap_src/push_swap.h"
 
-int	obtain_value(t_list *elem)
+int main(int argc, char **argv)
 {
-	return (*((int *)(elem->content)));
+    t_push_swap_stack *a;
+    t_push_swap_stack *b ;
+    char **args = argv;  // Default to argv unless we split
+    
+    a = NULL;
+    b  = NULL; 
+
+    if (argc < 2 || (argc == 2 && !argv[1][0])) // Handle cases with no input or empty argument
+        return (1);
+    if (argc == 2) // If a single argument is provided, split it into multiple arguments
+    {
+        args = ft_split(argv[1], ' ');
+        if (!args) // Handle memory allocation failure
+            return (1);
+    }
+
+    // Initialize stack 'a' with the provided arguments
+    if (!stack_a_init(&a, args + 1))
+    {
+        if (argc == 2)  // Free args only if it was split
+            free(args);
+        return (1);
+    }
+
+    // Perform sorting only if the stack is not already sorted
+    if (!sorted_stack(a))
+    {
+        size_t stack_len = length_stack(a);
+
+        if (stack_len == 2)
+            sa(&a, false);
+        else if (stack_len == 3)
+            alt_sorter(&a);
+        else
+            turks_sorter(&a, &b);
+    }
+
+    // Free the stacks and args if it was allocated by ft_split
+    free_mystack(&a);// to implement
+    free_mystack(&b);
+    if (argc == 2)
+        free(args);
+
+    return (0);
 }
 
-t_list	*string_covert_int(char *content)
-{
-	int	*tmp;
-
-	tmp = malloc(sizeof(int));
-	*tmp = ft_atoi(content);
-	return (ft_lstnew(tmp));
-}
-
-void	print_list(t_list *a)
-{
-	t_list	*temp_a;
-
-	temp_a = a;
-	while (temp_a != 0)
-	{
-		ft_printf("%d\n", obtain_value(temp_a));
-		temp_a = temp_a->next;
-	}
-}
-
-int	main(int argc, char **argv)
-{
-	t_list	*a;
-	t_list	*temp;
-	int		i;
-
-	a = NULL;
-	if (argc < 2 || argv[1][0] == '\0')
-		ft_error();
-	i = 1;
-	while (i < argc)
-	{
-		temp = string_covert_int(argv[i]);
-		if (ft_check_error(argv[i], temp) || ft_duplicate(a, ft_atoi(argv[i])))
-		{
-			ft_lstclear(&temp, free);
-			ft_lstclear(&a, free);
-			ft_error();
-		}
-		ft_lstadd_back(&a, temp);
-		i++;
-	}
-	if (sorted_checker(a) == 0)
-		sort_linklist(&a);
-	ft_lstclear(&a, free);
-	return (0);
-}
